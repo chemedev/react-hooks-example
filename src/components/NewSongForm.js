a7c4b46cd66051d31bc1975408cb1873141e56cc
mechell <mechell@pop-os.localdomain>
2020-06-23 23:17:26 -0300

    commit

---
+++ src/components/NewSongForm.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+export default ({ addSong }) => {
+  const [title, setTitle] = useState('');
+  const handleSubmit = e => {
+    e.preventDefault();
+    addSong(title);
+    setTitle('');
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Song name:</label>
+      <input
+        type="text"
+        required
+        value={title}
+        onChange={e => {
+          setTitle(e.target.value);
+        }}
+      />
+      <input type="submit" value="add song" />
+    </form>
+  );
+};
